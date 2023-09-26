be9ff6801a242881b5abc304bbf656d00e3c7d4f
johninkorea <john829829@gmail.com>
2023-09-26 09:36:05 +0900

    asd

--- 1_mlp.py
+++ 1_mlp.py
@@ -22,14 +22,31 @@ iris = datasets.load_iris()
 x=iris['data']
 y=iris['target']
 
-x_train, x_text, y_train, y_text = train_test_split(x,y,test_size=1-param.tr)
+x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=1-param.tr)
 
 
+x_train=torch.FloatTensor(x_train)
+x_test=torch.FloatTensor(x_test)
+y_train=torch.LongTensor(y_train)
+y_test=torch.LongTensor(y_test)
 
 
+class Net(nn.Module):
+    def __init__(self, in_ch, out_ch, num_node=[20,20]):
+        super(Net, self).__init__()
+    
+        self.relu = nn.ReLU()
+        self.sorftmax = nn.Softmax(dim=1)
 
+        self.fc1 = nn.Linear(in_ch, num_node[0])
+        self.fc2 = nn.Linear(num_node[0], num_node[1])
+        self.fc3 = nn.Linear(num_node[0], out_ch)
 
-
+    def forward(self, x):
+        x=self.relu(self.fc1(x))
+        x=self.relu(self.fc2(x))
+        x=self.sorftmax(self.sorftmax(x))
+        return x
 
 
 
