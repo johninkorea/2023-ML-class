558de849deac7fe353a2a0f070f57ff501f90c58
johninkorea <john829829@gmail.com>
2023-09-26 09:17:09 +0900

    asd

--- 1_mlp.py
+++ 1_mlp.py
@@ -2,13 +2,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import torch
 from torch.optim import adam
-
 from sklearn import datasets
+
 
 iris = datasets.load_iris()
 print(iris)
 
-
+data=iris['data']
+target=iris['target']
+# data=iris['data']
 
 
 
