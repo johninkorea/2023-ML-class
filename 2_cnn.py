77220f94078c6be6bc4f7b46f1b2c67a69977277
johninkorea <john829829@gmail.com>
2023-09-26 14:16:28 +0900

    asd

--- 2_cnn.py
+++ 2_cnn.py
@@ -3,87 +3,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 import torch
-import torch.optim as opt
 import torch.nn as nn
+import torch.optim as opt
+# import torchsummary as suammary
 
-from sklearn import datasets
-from sklearn.model_selection import train_test_split
-
-parse = argparse.ArgumentParser(description="asd")
-parse.add_argument("--epoch", default=100, type=int)
-parse.add_argument("--lr", default=.1, type=float)
-parse.add_argument("--tr", default=.6, type=float)
-
-param = parse.parse_args(args=[])
-
-iris = datasets.load_iris()
-# print(iris)
-
-x=iris['data']
-y=iris['target']
-
-x_train, x_test, y_train, y_test = train_test_split(x,y,test_size=1-param.tr)
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+from torch.utils.data import dataloader
 
 
-x_train=torch.FloatTensor(x_train)
-x_test=torch.FloatTensor(x_test)
-y_train=torch.LongTensor(y_train)
-y_test=torch.LongTensor(y_test)
 
 
-class Net(nn.Module):
-    def __init__(self, in_ch, out_ch, num_node=[20,20]):
-        super(Net, self).__init__()
-    
-        self.relu = nn.ReLU()
-        self.sorftmax = nn.Softmax(dim=1)
-
-        self.fc1 = nn.Linear(in_ch, num_node[0])
-        self.fc2 = nn.Linear(num_node[0], num_node[1])
-        self.fc3 = nn.Linear(num_node[1], out_ch)
-
-    def forward(self, x):
-        x=self.relu(self.fc1(x))
-        x=self.relu(self.fc2(x))
-        x=self.sorftmax(self.fc3(x))
-        return x
 
 
-model=Net(in_ch=4, out_ch=3)
-loss=nn.CrossEntropyLoss()
-optmizer=opt.Adam(model.parameters(), lr=param.lr)
 
 
-total_loss=[]
-acc_train=[]
-acc_test=[]
 
-model.train() #강속 받은 함수
-for epoch in range(param.epoch):
-    optmizer.zero_grad()
-
-    y_pred = model(x_train)
-    losses = loss(y_pred, y_train)
-    losses.backward()
-    optmizer.step()
-
-    total_loss.append(losses.data.item())
-
-    with torch.no_grad():
-        acc = (torch.argmax(y_pred, dim=1) == y_train).type(torch.FloatTensor)
-        acc_train.append(acc.mean().data.item())
-
-        y_test_pred = model(x_test)
-        acc = (torch.argmax(y_test_pred, dim=1) == y_test).type(torch.FloatTensor)
-        acc_test.append(acc.mean().data.item())
-    
-    if epoch % 10==0:
-        print(f"loss: {total_loss[-1]}")
-        print()
-
-plt.plot(acc_test)
-plt.plot(acc_test)
-plt.plot(total_loss)
 
 
 
