cc85e22ac63a1297923ad6b09341f979b371da4e
johninkorea <john829829@gmail.com>
2023-09-26 09:22:46 +0900

    asd

--- 1_mlp.py
+++ 1_mlp.py
@@ -1,17 +1,23 @@
+import argparse
 import numpy as np
 import matplotlib.pyplot as plt
 import torch
 from torch.optim import adam
 from sklearn import datasets
 
+parse = argparse.ArgumentParser(description="asd")
+parse.add_argument("--epoch", default=100, type=int)
+parse.add_argument("--lr", default=.001, type=float)
+parse.add_argument("--tr", default=.6, type=float)
+
 
 iris = datasets.load_iris()
-print(iris)
+# print(iris)
 
-data=iris['data']
-target=iris['target']
-# data=iris['data']
+x=iris['data']
+y=iris['target']
 
+x_train, x_text, y_train, y_text = train_text_split(x,y,test_size=1-parse.tr)
 
 
 
